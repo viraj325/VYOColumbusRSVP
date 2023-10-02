952e0fba40fdfbf6938824044d7e9b6b4a5b1b6e
Viraj Patel <virajpatel325@gmail.com>
2023-10-02 00:29:55 -0400

    code update

--- src/Sign.js
+++ src/Sign.js
@@ -20,8 +20,8 @@ const uiConfig = {
     // We will display Google and Facebook as auth providers.
     signInOptions: [
         firebase.auth.EmailAuthProvider.PROVIDER_ID,
-        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        firebase.auth.FacebookAuthProvider.PROVIDER_ID
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID
+        //firebase.auth.FacebookAuthProvider.PROVIDER_ID
     ]
 }
 
